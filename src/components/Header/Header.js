c03e528e35ff2073cde448d95edfd48e169a2765
Jubayer-Saad <61206194+Jubayer-Saad@users.noreply.github.com>
2020-09-09 22:43:48 +0600

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
+            <div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,6 +18,7 @@ const Header = () => {
     </div>
   </div>
 </nav>
+            </div>
         </div>
     );
 };
